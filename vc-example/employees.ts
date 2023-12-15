3e0c7a61c3f45572d4f319a4d51e5bd3932ba219
FeynmanFan <cbehrens@gmail.com>
2023-12-15 15:26:51 -0600

    Migrated employees to TypeScript

--- vc-example/employees.ts
+++ vc-example/employees.ts
@@ -4,11 +4,16 @@ class Employee {
         this.lastName = lastName;
         this.employeeId = employeeId;
     }
-
+	
+	firstName: string;
+	lastName: string;
+	employeeId: string;
+	
     FriendlyName() {
         return `${this.firstName} ${this.lastName}`;
     }
 }
+
 document.addEventListener("DOMContentLoaded", function () {
 	const employees = [
 		new Employee("John", "Doe", 1),
@@ -20,7 +25,7 @@ document.addEventListener("DOMContentLoaded", function () {
 		const table = document.createElement("table");
 		const headerRow = table.insertRow(0);
 
-		const headers = ["Employee ID", "First Name", "Last Name"];
+		const headers = ["Employee ID", "First Name", "Last Name", "Friendly Name"];
 		headers.forEach(headerText => {
 			const th = document.createElement("th");
 			th.textContent = headerText;
@@ -32,10 +37,12 @@ document.addEventListener("DOMContentLoaded", function () {
 			const cell1 = row.insertCell(0);
 			const cell2 = row.insertCell(1);
 			const cell3 = row.insertCell(2);
+			const cell4 = row.insertCell(3);
 
 			cell1.textContent = employee.employeeId;
 			cell2.textContent = employee.firstName;
 			cell3.textContent = employee.lastName;
+			cell4.textContent = employee.FriendlyName();
 		});
 
 		document.body.appendChild(table);
